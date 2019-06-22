e8958d142d980185e7d9c4bc915be2311c3253e9
Alejandra Torres <janicetr20@gmail.com>
2019-06-22 12:08:25 -0500

    Hice un Hello World

---
+++ cmake-build-debug/CMakeFiles/CLionProjectsPrueba.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLionProjectsPrueba.dir/main.cpp.obj"
+  "CLionProjectsPrueba.pdb"
+  "CLionProjectsPrueba.exe"
+  "CLionProjectsPrueba.exe.manifest"
+  "libCLionProjectsPrueba.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionProjectsPrueba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
